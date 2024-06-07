6501c689221caa8ad67f8b88aed87afbc804beac
Sergey Filyaev <s.filyaev@mail.ru>
2024-06-07 04:48:09 +0300

    Completed hw3

--- hw2/main.go
+++ hw2/main.go
@@ -1,26 +1,8 @@
-// package main
+package main
 
-// import (
-// 	"context"
-// 	"fmt"
-// 	"log"
-// 	"m/db"
+import "fmt"
 
-// 	"github.com/jackc/pgx/v5"
-// )
+func main() {
+	fmt.Println("Starting main process")
 
-// var queries *db.Queries
-// var ctx = context.Background()
-
-// func main() {
-// 	fmt.Println("Starting main")
-// 	fmt.Println("Starting db connection...")
-// 	conn, err := pgx.Connect(ctx, "user=postgres password=postgres dbname=bankstoredb sslmode=disable host=localhost port=5435")
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// 	defer conn.Close(ctx)
-// 	queries = db.New(conn)
-
-// // 	// fmt.Println(cards)
-// // }
+}
---
+++ hw3/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Starting main process")
+	// config, err := util.LoadConfig(".")
+	// if err != nil {
+	// 	log.Fatal("Can not read config file", err)
+	// }
+
+	// pool, err := pgxpool.New(context.Background(), config.DBSource)
+	// if err != nil {
+	// 	log.Fatal("Can not connect to db", err)
+	// }
+
+	// defer pool.Close()
+
+	// store := db.NewStore(pool)
+	// server, err := api.NewServer(config, store)
+	// if err != nil {
+	// 	log.Fatal("Can not create server", err)
+	// }
+
+	// err = server.Start(config.ServerAddress)
+	// if err != nil {
+	// 	log.Fatal("Can not start server", err)
+	// }
+
+}
